462efc8022b484a4d36a934a18d5aca816cdaa67
shipperizer <alexcabb@gmail.com>
2013-09-15 21:56:42 +0200

    Static pages fnished, back to master

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
-
+	it "should have the right title" do
+	  visit '/static_pages/home'
+	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+	end
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
@@ -11,7 +14,10 @@ describe "Static pages" do
   end
 
   describe "Help page" do
-
+  	it "should have the right title" do
+	  visit '/static_pages/help'
+	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+	end
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
@@ -19,10 +25,13 @@ describe "Static pages" do
   end
 
   describe "About page" do
-
-    it "should have the content 'About'" do
+	it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_content('About')
+      expect(page).to have_content('About Us')
+    end
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
     end
   end
 end
