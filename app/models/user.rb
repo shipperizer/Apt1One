5fefc6eb4a4818b3b28c2af1a0a50520a7f82033
shipperizer <alexcabb@gmail.com>
2013-10-04 15:36:05 +0200

    Start Chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	before_save {self.email = email.downcase }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	before_save { email.downcase! }
 	validates :name, presence: true, length: {maximum: 50}
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 	has_secure_password
